3db39ab8227d26b63d38767502477a6cb38db431
Chris Yates <chris.m.yates@outlook.com>
2023-02-14 14:55:14 -0500

    Fix unused imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
-import Contact from "./pages/Contact";
 import Navbar from "./components/Navbar";
-import Resume from "./pages/Resume";
 import NotFound from "./pages/NotFound";
 import Construction from "./pages/Construction";
 
