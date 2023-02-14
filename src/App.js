abf4eeab1fed9c2d6d852e6bb36c1b0c1dff94ef
Chris Yates <chris.m.yates@outlook.com>
2023-02-14 16:34:28 -0500

    Add easter egg for Emily

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import NotFound from "./pages/NotFound";
 import Construction from "./pages/Construction";
+import OneHundredMornings from "./pages/OneHundredMornings";
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
                 <Route path='/contact' element={<Construction />} />
                 <Route path='/resume' element={<Construction />} />
                 <Route path='/under-construction' element={<Construction />} />
+                <Route path='/9549026547173040731159' element={<OneHundredMornings />} />
                 <Route path='/not-found' element={<NotFound />} />
                 <Route path='*' element={<NotFound />} />
             </Routes>
---
+++ src/pages/OneHundredMornings.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import OHM from '../assets/ohm.jpeg'
+
+const OneHundredMornings = () => {
+    return (
+            <div>
+                <div className='mx-6 min-h-screen'>
+                    <img src={OHM} alt="Emily"/>
+                </div>
+            </div>
+    );
+};
+
+export default OneHundredMornings;
