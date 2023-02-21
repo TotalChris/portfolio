0501e23478cd16d077b1a5f319a7bff4b7d09881
TotalChris <chris.m.yates@outlook.com>
2023-02-20 22:52:55 -0500

    That was much easier than I thought

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,22 +1,9 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {Navigate, Outlet} from "react-router-dom";
-import {getAuth, onAuthStateChanged} from "firebase/auth";
+import {getAuth} from "firebase/auth";
 
 const PrivateRoute = () => {
-
-    const [loggedIn, setLoggedIn] = useState(false);
-
-    useEffect(() => {
-        onAuthStateChanged(getAuth(), (user) => {
-            if(user){
-                setLoggedIn(true)
-            } else {
-                setLoggedIn(false)
-            }
-        })
-    })
-
-    return (loggedIn ? <Outlet /> : <Navigate to='/log-in' />);
+    return (getAuth().currentUser ? <Outlet /> : <Navigate to='/log-in' />);
 };
 
 export default PrivateRoute;
