a410241347435310195da2ec5cd1e90bc921ad99
TotalChris <chris.m.yates@outlook.com>
2023-02-28 00:45:16 -0500

    Add subtitles to PostListing
    Fix a height issue with header thumbnails

--- src/components/PostListing.jsx
+++ src/components/PostListing.jsx
@@ -7,11 +7,12 @@ const PostListing = ({post, postId, handleFilterPush}) => {
     const dateFormatter = Intl.DateTimeFormat("en-US", {month: 'long', day: 'numeric', year: "numeric"})
 
     return (
-        <div className='w-full lg:w-96 h-24 lg:h-auto flex flex-row lg:flex-col'>
+        <div className='w-full lg:w-96 lg:h-auto flex flex-row lg:flex-col'>
             <Link to={'/posts/' + postId} className='w-36 lg:min-w-full'><img src={post.header} className='h-full object-cover rounded-xl w-full lg:h-52' alt='post header'/></Link>
             <div className='flex flex-col text-right lg:text-left ml-auto lg:ml-0 gap-2 max-w-2/3 lg:mt-4'>
                 <Link to={'/posts/' + postId} className='w-auto'><p className='text-xl font-bold'>{post.title}</p></Link>
-                <div className='flex flex-row gap-2 justify-end lg:justify-start'>
+                <Link to={'/posts/' + postId} className='w-auto'><p className='text-md text-neutral-500'>{post.subtitle}</p></Link>
+                <div className='flex flex-row gap-2 justify-end lg:justify-start mt-2'>
                     {post.tags.map((tag, i) => {
                         return <Tag text={tag} handleClick={handleFilterPush} key={i} className='hover:tag-invert'/>
                     })}
