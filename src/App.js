1b3ee94ac63c3ab7099dbde80c426a6a889bc641
Chris Yates <chris@chrisyates.dev>
2024-01-08 23:58:36 -0500

    Extract Routes to new data-routing method and bump react-router to latest
    Add blur transitions using react-transition-group
    Add transition code to App.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {
+	createBrowserRouter,
+	RouterProvider,
+	useLocation,
+    useOutlet,
+} from "react-router-dom";
+import { CSSTransition, SwitchTransition } from 'react-transition-group'
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import NotFound from "./pages/NotFound";
@@ -16,41 +22,63 @@ import Redirect from "./components/Redirect";
 import Posts from "./pages/Posts";
 import Profile from "./pages/Profile";
 import TitleNotes from "./pages/TitleNotes";
+import {createRef} from "react";
 
-function App() {
-
-  return (
-    <div className='app bg-white-200 dark:bg-black text-black dark:text-white min-h-screen'>
-        <Router>
+function NavFrame() {
+    const location = useLocation();
+    const outlet = useOutlet();
+    const { nodeRef } = routes.find((route) => route.path === location.pathname) ?? {}
+    return (
+        <div className='app bg-white-200 dark:bg-black text-black dark:text-white min-h-screen'>
             <Navbar />
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path="/title/" element={<Redirect to='https://titlenotes.netlify.app/' />} />
-                <Route path="/title/index.html" element={<Redirect to='https://titlenotes.netlify.app/' />} />
-                <Route path='/contact' element={<Contact />} />
-                <Route path='/contact/thank-you' element={<ThankYou />} />
-                <Route path='/resume' element={<Resume />} />
-                <Route path='/under-construction' element={<Construction />} />
-                <Route path='/log-in' element={<LogIn />} />
-                <Route path='/not-found' element={<NotFound />} />
-                <Route path='/title-notes' element={<TitleNotes />} />
-                <Route path="/log-out" element={<PrivateRoute />}>
-                    <Route path='/log-out' element={<LogOut />} />
-                </Route>
-                <Route path="/new-post" element={<PrivateRoute />}>
-                    <Route path='/new-post' element={<PostForm />} />
-                </Route>
-                <Route path="/preview/" element={<Redirect to='https://preview.chrisyates.dev' />} />
-                <Route path="/posts" element={<Posts />} />
-                <Route path="/posts/:postId" element={<Post />} />
-                <Route path="/profile/:userId" element={<Profile />} />
-                <Route path='*' element={<NotFound />} />
-            </Routes>
-        </Router>
-    </div>
-
-
-);
+            <SwitchTransition>
+                <CSSTransition
+                    key={location.pathname}
+                    nodeRef={nodeRef}
+                    timeout={700}
+                    classNames="route"
+                    unmountOnExit
+                    >
+                    {(state) => (
+                        <div ref={nodeRef} className="route">
+                            {outlet}
+                        </div>
+                    )}
+				</CSSTransition>
+            </SwitchTransition>
+        </div>
+        )
 }
 
-export default App;
+const routes = [
+    {name: 'Under Construction', ref: createRef(), path: '!', Component: Construction},
+    {name: 'Home', ref: createRef(), path: '/', Component: Home},
+    {name: 'Resume', ref: createRef(), path: '/resume', Component: Resume},
+    {name: 'Not Found', ref: createRef(), path: '*', Component: NotFound},
+    {name: 'Contact', ref: createRef(), path: '/contact', Component: Contact},
+    {name: 'Thanks', ref: createRef(), path: '/contact/thank-you', Component: ThankYou},
+    {name: 'Log In', ref: createRef(), path: '/log-in', Component: LogIn},
+    {name: 'Log Out', ref: createRef(), path: '/log-out', Component: PrivateRoute, children: [ {path: '/log-out', Component: LogOut} ]},
+    {name: 'Profile', ref: createRef(), path: '/profile:userId', Component: Profile},
+    {name: 'New Post', ref: createRef(), path: '/new-post', Component: PrivateRoute, children: [ {path: '/new-post', Component: PostForm} ]},
+    {name: 'Posts', ref: createRef(), path: '/posts', Component: Posts},
+    {name: 'Post', ref: createRef(), path: '/posts/:postId', Component: Post},
+    {name: 'Title Notes', ref: createRef(), path: '/title-notes', Component: TitleNotes},
+    {name: 'Title Notes App', ref: createRef(), path: '/title', Component: () => <Redirect to='https://titlenotes.netlify.app/'></Redirect>},
+    {name: 'Title Notes App Home', ref: createRef(), path: '/title/index.html', Component: () => <Redirect to='https://titlenotes.netlify.app/'></Redirect>},
+
+]
+
+const router = createBrowserRouter([
+    {Component: NavFrame, children: routes.map((route) => ({
+        path: route.path,
+        Component: route.Component,
+        children: route.children ?? [],
+    }))}
+]);
+
+export default function App() {
+    return (
+        <RouterProvider router={router} />
+    )
+};
