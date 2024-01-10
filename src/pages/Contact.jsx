27cde4c12595922b6cfb1aa8c5efaf8dba9a3859
Chris Yates <chris@chrisyates.dev>
2024-01-09 21:48:11 -0500

    Add form validation step to ensure blank responses aren't being sent
    Add Scale in and out to page transition

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import {useState} from 'react';
 import { useForm, ValidationError } from '@formspree/react';
 import {useNavigate} from "react-router-dom";
 import Spinner from "../components/Spinner";
@@ -15,6 +15,8 @@ const Contact = () => {
         inquiry: '',
     })
 
+    const [error, setError] = useState(false)
+
     const { name, contact, inquiry } = formData;
 
     const handleChange = (e) => {
@@ -26,14 +28,23 @@ const Contact = () => {
         })
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(formData.name.length > 3 && formData.contact.length > 5 && formData.inquiry.length > 30){
+            formSubmit(e).then()
+        } else {
+            setError(true)
+        }
+    }
+
     if(formState.succeeded){
-        navigate('/contact/thank-you')
+        navigate('/contact/thank-you');
     }
 
     return (
         <div className='pt-24 mx-auto px-4 pb-4' style={{fontFamily: 'Roboto Mono', maxWidth: '1080px'}}>
             <h1 className='text-6xl'>Contact Me</h1>
-            <form method="POST" onSubmit={formSubmit} className='w-full'>
+            <form method="POST" onSubmit={handleSubmit} onFocus={() => {setError(false)}} className='w-full'>
             <p className='text-xl mt-8'>Feel free to reach out to me with any potential projects or employment opportunites that you think would fit my skill set!</p><br/><p> If you want more info about my existing work, check out my <Link to='/resume' className='underline hover:cursor-pointer'>resume</Link>.</p>
 
                 <input type="hidden" name="form-name" value="contact" />
@@ -47,7 +58,10 @@ const Contact = () => {
                     <textarea name="inquiry" id="inquiry" value={inquiry} onChange={handleChange} className="rounded-2xl textarea w-full bg-transparent border-black text-black dark:border-white dark:text-white focus:outline-none h-64 md:h-48 focus:border-black focus:dark:border-white" placeholder="Inquiry"></textarea>
                     <ValidationError prefix="Inquiry" field="inquiry" errors={formState.errors} />
                 </div>
-                <div className='flex flex-row gap-8 mt-4'>
+                <div className={ 'w-full flex flex-row gap-8 mt-4 collapse ' + (error ? 'collapse-open' : '') }>
+                    <div className='collapse-content text-red-700 my-auto p-0 flex-grow'>
+                        Your information isn&apos;t complete. Try again.
+                    </div>
                     <button type='submit' disabled={formState.submitting} className='ml-auto btn-wire'>
                         {formState.submitting ? <Spinner /> : 'Send'}
                     </button>
