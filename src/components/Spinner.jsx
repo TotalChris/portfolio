52cb3b13b7df74c8e092afaa226769f47d68dbdb
Chris Yates <chris@chrisyates.dev>
2024-02-08 18:00:54 -0500

    Expand Loading Spinner to a full height and width and centering
    Fix small layout issue on Project cards

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -3,8 +3,8 @@ import spinner from '../assets/spinner.gif'
 
 const Spinner = () => {
     return (
-        <div className=''>
-            <img src={spinner} height="32px" width="32px" alt="loading" />
+        <div className='flex justify-center items-center h-96'>
+            <img src={spinner} height="64px" width="64px" alt="loading" style={{height: '64px', width: '64px'}} />
         </div>
     );
 };
